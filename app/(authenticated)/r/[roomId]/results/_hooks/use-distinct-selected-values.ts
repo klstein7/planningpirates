d0b2f23319231523f775e77c0149f8bf096d7c17
klstein7 <klstein7@gmail.com>
2023-09-18 20:20:57 -0600

    fix sorting in results

--- app/(authenticated)/r/[roomId]/results/_hooks/use-distinct-selected-values.ts
+++ app/(authenticated)/r/[roomId]/results/_hooks/use-distinct-selected-values.ts
@@ -1,11 +1,20 @@
 import { useMemo } from "react";
 import { useSelectedValues } from "./use-selected-values";
 
+const getVotesForSelectedValue = (selectedValues: number[], value: number) => {
+  return selectedValues.filter((v) => v === value).length;
+};
+
 export const useDistinctSelectedValues = () => {
   const { selectedValues } = useSelectedValues();
 
   const distinctSelectedValues = useMemo(() => {
-    return [...new Set(selectedValues)];
+    return [...new Set(selectedValues)].sort((a, b) => {
+      return (
+        getVotesForSelectedValue(selectedValues, b) -
+        getVotesForSelectedValue(selectedValues, a)
+      );
+    });
   }, [selectedValues]);
 
   return {
